1ca202db7c1e10378adaf9d6bc833477266644a4
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2022-07-23 18:37:17 -0400

    feat: new welcome script

---
+++ scripts/welcome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo
+echo "--------------------------"
+echo " Welcome ${USER}"
+echo
+echo " Loaded ~/.zhrc"
+echo " Edit aliases: edit"
+echo " To refresh run: fresh"
+echo " View all aliases: alias"
+echo "--------------------------"
+echo
