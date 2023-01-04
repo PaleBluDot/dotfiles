387604ffa161fd363390097c9a1631f910c02dc1
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2023-01-04 14:09:06 -0500

    feat: linux setup

---
+++ bin/linuxSetup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+########################
+## 1. task 1
+## 2. task 2
+########################
+
+dotfiles=$HOME/.config/dotfiles
+
+if [[ ! -d $dotfiles ]]
+then
+	git clone https://github.com/PaleBluDot/dotfiles.git $dotfiles
+fi
+
+if [[ ! -d $HOME/.ssh/ ]]
+then
+	mkdir $HOME/.ssh
+fi
+
+link_files() {
+	ln -fs $HOME/.config/dotfiles/.config/authorized ~/.ssh/authorized_keys
+	ln -fs $HOME/.config/dotfiles/.config/npm-globals.txt ~/.config/npm-globals.txt
+
+
+	ln -fs $HOME/.config/dotfiles/.config/configstore/ ~/.config/configstore
+	ln -fs $HOME/.config/dotfiles/.vscode/ ~/.vscode
+	ln -fs $HOME/.config/dotfiles/bin/ ~/bin
+
+
+	ln -fs $HOME/.config/dotfiles/.aliases ~/.aliases
+	ln -fs $HOME/.config/dotfiles/.bashrc ~/.bashrc
+	ln -fs $HOME/.config/dotfiles/.gitconfig ~/.gitconfig
+	ln -fs $HOME/.config/dotfiles/.nanorc ~/.nanorc
+	ln -fs $HOME/.config/dotfiles/.npmrc ~/.npmrc
+	ln -fs $HOME/.config/dotfiles/.p10k.zsh ~/.p10k.zsh
+	ln -fs $HOME/.config/dotfiles/.zshrc ~/.zshrc
+	echo "all config files linked"
+}
+
+link_files
+
+echo
+echo "Completed on $(date +%c)"
