453f743d6f103c4018f98692e9ed31be14bb2103
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2023-01-05 15:52:12 -0500

    chore: refactored

--- bin/linuxSetup.sh
+++ bin/linuxSetup.sh
@@ -41,10 +41,11 @@ link_files() {
 }
 
 install_packages() {
-	PACKAGES=(zsh zip unzip fontconfig)
-	SERVICES=(zsh zip unzip fc-list)
+	PACKAGES=("zsh" "zip" "fontconfig")
+	SERVICES=("zsh" "zip" "fc-list")
 	FILENAME=$0
 	NAME="$(basename $FILENAME)"
+	LOCATION=$(command -v ${SERVICES[@]})
 
 	if [ ! "$EUID" -eq 0 ]
 		then echo "Please run as root"
@@ -53,24 +54,20 @@ install_packages() {
 
 	for package in ${PACKAGES[@]}
 	do
-		if ! command -v $SERVICES &> /dev/null
+		if ! command -v ${package[@]} &> /dev/null
 		then
 			echo -e "[-] Installing ${YELLOW}${package}${NC} [-]"
 			apt install $package -y &> /dev/null
-			echo
-			echo -e "$package installed on ${CYAN}$(date +%c)${NC}"
+			echo -e "$package was installed."
 			echo
 		else
-			echo -e "${YELLOW}${package}${NC} already install"
+			echo -e "${YELLOW}${package}${NC} is already install."
 		fi
 	done
-
-	echo
-	echo -e "Completed ${YELLOW}${NAME^}${NC} on ${CYAN}$(date +%c)${NC}"
 }
 
 link_files
 install_packages
 
 echo
-echo "Completed on $(date +%c)"
+echo -e "Completed ${YELLOW}${NAME^}${NC} on ${CYAN}$(date +%c)${NC}"
