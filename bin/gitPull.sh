d9182ed708f1b6a0dde1b59d596683c07e111382
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2023-01-04 19:23:31 -0500

    feat: moved colors

--- bin/gitPull.sh
+++ bin/gitPull.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-REPO=$(git config --get remote.origin.url)
-echo -e "Updating ${YELLOW}${REPO}${NC}..."
-git pull
-echo
+if [ -d .git ] &> /dev/null
+then
+	REPO=$(git config --get remote.origin.url)
+	echo -e "Updating ${YELLOW}${REPO}${NC}..."
+	git pull
+	echo
+fi
