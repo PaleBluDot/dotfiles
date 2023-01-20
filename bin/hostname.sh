9a75fccf43bbb13247be08b360a777a7b37afdea
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2023-01-20 14:07:25 -0500

    fix: find and replace as well

--- bin/hostname.sh
+++ bin/hostname.sh
@@ -9,13 +9,22 @@ read -p "Do you want change hostname? (y/n) " yn
 
 case $yn in
 	[yY] )
-		read -p "Enter hostname? " test
-		sudo hostnamectl set-hostname $test
-		echo "hostname is now $(hostname)"
+		old_hn=$(hostname)
+		echo "Old hostname: $old_hn"
+		read -p "Enter hostname? " new_hn
+
+		sudo sed -i "s/$old_hn/$new_hn/g" /etc/hosts
+		sudo hostnamectl set-hostname $new_hn
+		sudo hostnamectl set-hostname "${new_hn^}" --pretty
+
+		echo "Hostname is now:  $(hostname)"
+		echo "/etc/host is now: $(cat /etc/hosts | grep $new_hn)"
+
+		echo "please reboot"
 		;;
 	[nN] )
-		echo "Skipping step";
-		;;
+		echo "Exiting....";
+		exit 0;;
 	* )
 		echo "invalid response";
 		exit 1;;
