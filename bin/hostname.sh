b242b48c9e10c8134911147bcba36aba795d0e2f
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2023-01-20 15:11:30 -0500

    fix: remove \n

--- bin/hostname.sh
+++ bin/hostname.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-## 1. update hostname
 ##################################################
-# This section is for system, user, and
-# group settings.
+# This section is for updating hostname.
 ##################################################
 old_hn=$(hostname)
 echo "Old hostname: $old_hn"
 read -p "Enter hostname? " new_hn
-
 echo "Replacing $old_hn with $new_hn"
 sed -i "s/$old_hn/$new_hn/g" /etc/hosts
 hostnamectl set-hostname $new_hn
-hostnamectl set-hostname "${new_hn^}" --pretty
-
+# hostnamectl set-hostname "${new_hn^}" --pretty
 echo "Hostname is now: $(hostname)"
-
 echo "Please reboot!"
