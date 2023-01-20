11e1dba2d8eaa8f9a4eb17f1acebe6088a346c93
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2023-01-20 14:41:22 -0500

    fix: clean up code

--- bin/hostname.sh
+++ bin/hostname.sh
@@ -5,27 +5,15 @@
 # This section is for system, user, and
 # group settings.
 ##################################################
-read -p "Do you want change hostname? (y/n) " yn
+old_hn=$(hostname)
+echo "Old hostname: $old_hn"
+read -p "Enter hostname? " new_hn
 
-case $yn in
-	[yY] )
-		old_hn=$(hostname)
-		echo "Old hostname: $old_hn"
-		read -p "Enter hostname? " new_hn
+echo "Replacing $old_hn with $new_hn"
+sudo sed -i "s/$old_hn/$new_hn/g" /etc/hosts
+sudo hostnamectl set-hostname $new_hn
+sudo hostnamectl set-hostname "${new_hn^}" --pretty
 
-		sudo sed -i "s/$old_hn/$new_hn/g" /etc/hosts
-		sudo hostnamectl set-hostname $new_hn
-		sudo hostnamectl set-hostname "${new_hn^}" --pretty
+echo "Hostname is now: $(hostname)"
 
-		echo "Hostname is now:  $(hostname)"
-		echo "/etc/host is now: $(cat /etc/hosts | grep $new_hn)"
-
-		echo "please reboot"
-		;;
-	[nN] )
-		echo "Exiting....";
-		exit 0;;
-	* )
-		echo "invalid response";
-		exit 1;;
-esac
+echo "Please reboot!"
