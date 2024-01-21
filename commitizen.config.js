3e09718c186899070a63cce000ed14b6ff6394a4
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2024-01-21 13:27:08 -0500

    feat(settings): add commitlint rules and commitzen cli tool
    ISSUES CLOSED: #5

---
+++ commitizen.config.js
@@ -0,0 +1,52 @@
+const types = [
+	{
+		value: "ci",
+		name: "⚙️ ci: CI configuration files and scripts",
+	},
+	{
+		value: "chore",
+		name: "🔩 chore: Doesn't modify src files",
+	},
+	{
+		value: "docs",
+		name: "📚 docs: Update to the documentation",
+	},
+	{
+		value: "feat",
+		name: "✨ feat: A new feature",
+	},
+	{
+		value: "fix",
+		name: "🐞 fix: A bug fix",
+	},
+	{
+		value: "refactor",
+		name: "♻  refactor: Neither fixes nor adds a feature",
+	},
+	{
+		value: "revert",
+		name: "⏪ revert: Reverts a previous commit",
+	},
+];
+
+const scopes = ["config", "lib", "script", "settings"].map((name) => ({
+	name,
+}));
+
+module.exports = {
+	types,
+	scopes,
+	messages: {
+		type: "Type of change that you're committing:",
+		scope: "\nSCOPE of this change (optional):",
+		customScope: "Custom SCOPE of this change:",
+		subject: "SHORT, IMPERATIVE tense description:\n",
+		body: "LONGER description change (optional):\n",
+		breaking: "BREAKING CHANGES (optional):\n",
+		footer: "CLOSED ISSUES (optional):\n",
+		confirmCommit: "Are you sure you want to commit?",
+	},
+	allowCustomScopes: false,
+	allowBreakingChanges: false,
+	subjectLimit: 60
+};
