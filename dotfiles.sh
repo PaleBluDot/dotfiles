e4f1957d665162781bf1b1dccde2713730a760b2
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2026-03-21 17:56:01 -0400

    fix(script): bootstrap with os detection and yq install

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,8 +1,63 @@
 #!/usr/bin/env zsh
 
+##########################
+##############@ VARIABLES
+##########################
 DRY_RUN=false
 VERBOSE=false
 
+
+##########################
+##############@ FUNCTIONS
+##########################
+_detect_os(){
+  case "$(uname)" in
+    Darwin)
+      echo "darwin"
+      ;;
+    Linux)
+      if grep -qi "microsoft" /proc/version; then
+        echo "wsl"
+      else
+        echo "linux"
+      fi
+      ;;
+    MINGW*|MSYS*|CYGWIN*)
+      echo "windows-native"
+      ;;
+    *)
+      echo "Error: unsupported OS '$(uname)'" >&2
+      exit 1
+      ;;
+  esac
+}
+
+_bootstrap() {
+  DOTFILES=$(cd "$(dirname "$0")" && pwd)
+
+  if command -v yq &>/dev/null; then
+    echo "yq is installed"
+  else
+    echo "yq is not installed"
+    echo "installing yq..."
+    case "$(_detect_os)" in
+      darwin)
+        brew install yq
+        ;;
+      linux|wsl)
+        sudo apt-get install yq
+        ;;
+      windows-native)
+        winget install yq
+        ;;
+    esac
+  fi
+}
+
+
+##########################
+##################@ FLAGS
+##########################
 for arg in "$@"; do
   case "$arg" in
     --dry-run)
@@ -16,26 +71,4 @@ for arg in "$@"; do
   esac
 done
 
-_detect_os(){
-  case "$(uname)" in
-    Darwin)
-      echo "darwin"
-      ;;
-    Linux)
-      if grep -qi "microsoft" /proc/version; then
-        echo "wsl"
-      else
-        echo "linux"
-      fi
-      ;;
-    Windows)
-      if [[ -n $COMSPEC ]]; then
-        echo "windows-native"
-      fi
-      ;;
-    *)
-      echo "Error: unsupported OS '$(uname)'" >&2
-      exit 1
-      ;;
-  esac
-}
+
